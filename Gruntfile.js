0f1d9cc28138e556dbcc458c0f51109699f4bd9b
Gabriel Baciu <gabriel.g.baciu@gmail.com>
2013-12-08 18:29:00 -0500

    uglify and minify via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,11 +23,40 @@ module.exports = function (grunt) {
           config: ".jsbeautifyrc"
         }
       }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          "public/javascripts/build/app.min.js": [
+            "public/javascripts/bower_components/jquery/jquery.js",
+            "public/javascripts/bower_components/underscore/underscore.js",
+            "public/javascripts/bower_components/backbone/backbone.js",
+            "public/javascripts/bower_components/async/lib/async.js",
+            "public/javascripts/bower_components/mustache/mustache.js",
+            "public/javascripts/app/window.js"
+          ]
+        }
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: "/* My minified css file */"
+        },
+        files: {
+          "public/css/build/style.min.css": ["public/css/window.css"]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-jsbeautifier");
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
   grunt.registerTask("clean", [
     "jsbeautifier:modify",
@@ -37,6 +66,11 @@ module.exports = function (grunt) {
   grunt.registerTask("verify", [
     "jsbeautifier:verify",
     "jshint"
+  ]);
+
+  grunt.registerTask("build", [
+    "cssmin",
+    "uglify"
   ]);
 
 };
